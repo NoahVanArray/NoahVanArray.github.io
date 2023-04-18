9878e216c041f90cf13a1162855f880e49731e2c
yoe <johnyohannavarra@gmail.com>
2023-04-18 14:58:15 +0800

    brain hurts
    still fixing the login/registration page

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,8 +5,6 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=> {
 	wrapper.classList.add('active');
@@ -16,10 +14,3 @@ loginLink.addEventListener('click', ()=> {
 	wrapper.classList.remove('active');
 });
 
-btnPopup.addEventListener('click', ()=> {
-	wrapper.classList.add('active-popup');
-});
-
-iconClose.addEventListener('click', ()=> {
-	wrapper.classList.remove('active-popup');
-});
